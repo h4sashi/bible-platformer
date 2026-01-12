55b77f6805049911cd41a343fa69732f006edc4a
Tamar <westtmu@gmail.com>
2026-01-12 16:42:43 +0100

    Typing Sentence Feature

--- Assets/Scripts/BibleTrigger.cs
+++ Assets/Scripts/BibleTrigger.cs
@@ -1,23 +1,67 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BibleTrigger : MonoBehaviour
 {
-    
+    [TextArea]
     public string verse;
-    public TextMeshProUGUI verseText;
 
+    public TextMeshProUGUI verseText;
     public GameObject bottomContainer;
 
+    public float typingSpeed = 0.04f;
+
+    private Coroutine typingRoutine;
+    private bool hasTriggered;
+
+    public event Action OnTypingFinishEvent;
+
+    private void OnEnable() {
+        OnTypingFinishEvent += DisableUI;
+    }
+
+    private void DisableUI()
+    {
+        StartCoroutine(delayDisableUI());
+    }
+
+    IEnumerator delayDisableUI()
+    {
+        yield return new WaitForSeconds(3f);
+        verseText.text = "";
+        bottomContainer.SetActive(false);
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (!other.CompareTag("Player"))
+            return;
+        if (hasTriggered)
+            return;
+
+        hasTriggered = true;
+
+        bottomContainer.SetActive(true);
+
+        if (typingRoutine != null)
+            StopCoroutine(typingRoutine);
+
+        typingRoutine = StartCoroutine(TypeVerse());
+    }
+
+    IEnumerator TypeVerse()
+    {
+        verseText.text = "";
+
+        foreach (char c in verse)
         {
-            bottomContainer.SetActive(true);
-            verseText.text = verse;
+            verseText.text += c;
+            yield return new WaitForSeconds(typingSpeed);
         }
+
+        OnTypingFinishEvent?.Invoke();
     }
 }
